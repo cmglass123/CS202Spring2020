46c4b8402bd27305e0f946977a248679063697d0
Christopher Glass <cmglass@alaska.edu>
2020-01-19 21:25:04 -0900

    added some includes that we will be using

---
+++ HomeWork/HW00/HW00.cpp
@@ -0,0 +1,16 @@
+/*
+Christopher Glass
+HW00 Temperature
+CS202 Spring 2020
+*/
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+
+int main(int argc, char* argv[])
+{
+
+}
