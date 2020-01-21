2444fc5ff6161e9581fceffe92da7c4783488ab9
cmglass <cmglass@alaska.edu>
2020-01-21 11:03:26 -0900

    made a do while and switch statment

--- Labs/Lab01/Lab01.cpp
+++ Labs/Lab01/Lab01.cpp
@@ -7,7 +7,29 @@ Lab #1
 
 #include <iostream>
 
+void foo()
+{
+
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	int x;
+	std::cout << "How many suits do you want: ";
+	std::cin >> x;
+
+	do 
+	{
+		switch (x)
+		{
+		case 1:
+			std::cout << "thanks for buying " << x << "suits";
+		case 2:
+			std::cout << "thanks for buying " << x << "suits";
+		default:
+			break;
+		}
+	} while (x != ' ');
+
+	if(x)
 }
