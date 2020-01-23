89fa714dab37e6201bd901309438a7514690b24a
cmglass <cmglass@alaska.edu>
2020-01-23 10:37:47 -0900

    made a class called Foo in the Lab02 header file

---
+++ Labs/Lab02/Header.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef Lab02
+#define Lab02
+
+class Foo
+{
+public:
+
+private:
+	
+};
+
+#endif // !Lab02
---
+++ Labs/Lab02/Lab02.cpp
@@ -0,0 +1,13 @@
+/*
+Christopher Glass
+Lab #02 
+1/23/20 spring 2020
+*/
+
+#include <iostream>
+
+int main()
+{
+
+}
+
