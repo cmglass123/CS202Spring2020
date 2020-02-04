dcbd46517bd6f518328cd2c8d822f129c6951e10
cmglass <cmglass@alaska.edu>
2020-02-04 11:05:55 -0900

    made a new text file that will have new things added at the end because of  the ios::app

---
+++ Labs/Lab04/Lab04.cpp
@@ -0,0 +1,13 @@
+// Lab04.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+	std::ofstream files("text.txt",std::ios::app);
+
+	std::ifstream ifiles("text.txt");
+
+}
