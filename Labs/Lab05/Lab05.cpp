0ee1562cdccb1d04086c9af0ffbd5b2c070ab992
cmglass <cmglass@alaska.edu>
2020-02-11 11:01:30 -0900

    made a if statement that opens the ofile and use seek to go the end and start of the file while tell says were it is at at that time

--- Labs/Lab05/Lab05.cpp
+++ Labs/Lab05/Lab05.cpp
@@ -11,5 +11,14 @@ int main()
 {
 	std::ifstream ofile("data.dat",std::ios::binary);
 
-	
+	if (ofile)
+	{
+		ofile.seekg(0, ofile.end);
+		int place = ofile.tellg();
+		ofile.seekg(0, ofile.beg);
+
+
+
+	}
+
 }
