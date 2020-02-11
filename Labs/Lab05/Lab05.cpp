7c30431445bbbd08e981656e483da8ec0df5e928
cmglass <cmglass@alaska.edu>
2020-02-11 11:24:51 -0900

    ok

--- Labs/Lab05/Lab05.cpp
+++ Labs/Lab05/Lab05.cpp
@@ -9,16 +9,38 @@ Binary Files
 
 int main()
 {
-	std::ifstream ofile("data.dat",std::ios::binary);
+	std::ifstream ofile("data.dat", std::ios::binary);
 
 	if (ofile)
 	{
-		ofile.seekg(0, ofile.end);
-		int place = ofile.tellg();
-		ofile.seekg(0, ofile.beg);
+		//ofile.seekg(0, ofile.end);
+		//int place = ofile.tellg();
+		//ofile.seekg(0, ofile.beg);
+
+		
+		int sum = 0;
+		int num = 0;
+		int average;
+
+		int x;
+
+		while (true)
+		{
+			
+			ofile.read(reinterpret_cast<char*>(&x), sizeof(int));
+			sum += x;
+			num++;
+
+			if (!ofile)
+				break;
+		}
 
 
+		average = double(double(sum) / num);
 
+		std::cout << sum << std::endl;
+		std::cout << num << std::endl;
+		std::cout << average << std::endl;
 	}
-
+	return 0;
 }
