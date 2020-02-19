a289a2b66fab5fc5b2aadc86184b95bf42ce8f76
Christopher Glass <cmglass@alaska.edu>
2020-02-18 17:26:52 -0900

    finished the program with a while loop

--- HomeWork/HW03/HW03.cpp
+++ HomeWork/HW03/HW03.cpp
@@ -16,4 +16,18 @@ int main() {
 	int col = 1;
 	string token;
 	ifstream ifile("Text.txt");
-	
+	while (std::getline(ifile, token)) {
+		std::istringstream line(token);
+		while (line >> token) {
+			cout << "line: " << new_line << " " << "collum: " << col << " " << token << std::endl;
+			col++;
+		}
+		if (ifile.unget().get() == '\n') {
+			col = 1;
+			cout << new_line << " newline" << endl;
+			new_line++;
+		}
+	}
+
+	return 0;
+}
